82ba4b9255187013d806b686561b43a36cbe9924
mcwa5631 <mcwa5631@mylaurier.ca>
2025-02-09 17:32:47 -0500

    finished

---
+++ 169065631_Python Lab 5/Activity 4.py
@@ -0,0 +1,12 @@
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+concatenated_list = list1 + list2
+print("Concatenated list using + operator:", concatenated_list)
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+list1 += list2
+print("Concatenated list using += operator:", list1)
+
+list1 = [1, 2, 3]
+number = 4
